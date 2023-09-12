6ee3f87b07920f79c0d1afcfe96b159cd2cf5ee2
Ayush <ayush.ayu2002@gmail.com>
2023-09-12 23:54:15 +0530

    data added

--- app/src/main/java/com/example/myapplication/Deatils_Form.java
+++ app/src/main/java/com/example/myapplication/Deatils_Form.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Deatils_Form extends AppCompatActivity {
     EditText name,section,rollno,id;
@@ -26,7 +35,27 @@ public class Deatils_Form extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(Deatils_Form.this, "Done", Toast.LENGTH_SHORT).show();
+
+                Map<String ,String> map = new HashMap<>();
+                map.put("Name",name.getText().toString());
+                map.put("Section",section.getText().toString());
+                map.put("Roll no",rollno.getText().toString());
+                map.put("Student Id",id.getText().toString());
+
+                try {
+
+                   FirebaseFirestore.getInstance().collection("Student Data").add(map).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentReference> task) {
+                            Toast.makeText(Deatils_Form.this, "Done", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                catch (Exception e){
+                    e.toString();
+                }
+
+
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
