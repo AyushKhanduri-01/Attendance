525b40db4077c5b92bf2078d72e99f262339de22
Ayush <ayush.ayu2002@gmail.com>
2024-02-29 21:08:10 +0530

    camera added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.myapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -27,6 +27,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    viewBinding {
+        viewBinding.isEnabled = true
+    }
 }
 
 dependencies {
@@ -39,6 +42,7 @@ dependencies {
     implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     implementation("com.google.firebase:firebase-firestore-ktx:24.7.1")
     implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -52,5 +56,21 @@ dependencies {
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 
     implementation("com.android.volley:volley:1.2.1")
+
+
+    // changes
+    implementation ("androidx.camera:camera-core:1.2.2")
+    implementation ("androidx.camera:camera-lifecycle:1.2.2")
+    implementation("androidx.camera:camera-camera2:1.2.2")
+    implementation ("androidx.camera:camera-video:1.2.2")
+    implementation ("androidx.camera:camera-view:1.2.2")
+    //ml kit
+    implementation("com.google.android.gms:play-services-mlkit-face-detection:17.1.0")
+    implementation ("com.google.android.gms:play-services-vision:20.1.1")
+    implementation ("com.google.android.gms:play-services-vision-common:19.1.1")
+    implementation("com.google.firebase:firebase-ml-vision:24.1.0")
+    //end ml kit
+    //  changes
+
 
 }
